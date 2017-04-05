81067401e907934420c9eb1809925f195d665584
Malcolm Meyer <malcolmtmeyer@gmail.com>
2017-04-04 20:33:33 -0400

    Create baselayer.js

---
+++ _includes/leaflet/baselayer.js
@@ -0,0 +1,6 @@
+/*Easily add basemaps or baselayers with L.tileLayer wms layers can also be added with L.tileLayer.wms */
+
+var OpenStreetMap_HOT = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+}).addTo(map);
