324bc41e2132c0b8e68a98e1b05df3971bb9c8af
unknown <mmeyer@MMEYERS-WS.ovrdc.local>
2017-05-04 15:29:43 -0400

    updates

--- _includes/leaflet/choropleth.js
+++ _includes/leaflet/choropleth.js
@@ -21,5 +21,27 @@ cData.on("ready", function() {
       layer.bindTooltip(feature.properties.NAME + '<' + 'br' + '>' + +feature.properties.POPULATION)
     }
   }).addTo(map);
+
   layerControl.addOverlay(choropleth, "Choropleth");
+
+  /*Legend Custom Leaflet Control*/
+  var legend = L.control({ position: 'bottomright' });
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var colors = choropleth.options.colors
+    var labels = []
+
+    /* Add min & max*/
+    div.innerHTML = '<div><h3 style="font-weight:bolder;font-size:larger;">Population Density</h3><br></div><div class="labels"><div class="min">Low</div> \
+  <div class="max">High</div></div>'
+
+  for (i = 1; i < colors.length; i++) {
+      labels.push('<li style="background-color: ' + colors[i] + '"></li>')
+    }
+
+    div.innerHTML += '<ul style="list-style-type:none;display:flex">' + labels.join('') + '</ul>'
+    return div
+  }
+
+  legend.addTo(map);
 });
